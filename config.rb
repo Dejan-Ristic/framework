e001130c2fac498630e6f058057ad8bb52e94d9c
Dejan Ristic <dristic79@gmail.com>
2015-11-09 18:38:47 +0100

    first_commit

---
+++ config.rb
@@ -0,0 +1,36 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "assets/img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
+
+sourcemap = (environment == :production) ? false : true
+
+
+
+
+# Additional Methods
+
+module Sass::Script::Functions
+    # Does the supplied image exist?
+    def file_exists(image_file)
+      path = image_file.value
+      Sass::Script::Bool.new(File.exists?(path))
+    end
+end
